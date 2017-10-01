6daee96305ee8505e8f9ee7e6a7bae7dd176b727
k0ryan <antoniosolanavera@gmail.com>
2017-10-01 20:24:39 +0200

    0.1.1

--- compiler.py
+++ compiler.py
@@ -1,17 +1,74 @@
 import sys
-import  lib00 as l
+import lib
 
-def cleann(file):
-    line = file.read().split("\n")
-    for x in range(0, len(line)):
-        if "\t" in line:
-            temp = line.split("t", 1)
-            print(temp)
+def clean(file0): # divides file in lines and instructions
+    line = file0.read().split("\n")
+    inst = []
+    for x in line:
+        inst.extend(x.split())
+
+    return inst
+
+def sovpile(inst): # creates file compiled into python
+    file0 = open(sys.argv[1].split(".")[0] + ".py", "w+")
+    for inst_name in inst:
+        if inst_name == "in":
+            sov = True
+            pos = lib.position(inst_name, inst) + 1
+            while sov:
+                if inst[pos] == "in.":
+                    sov = False
+                else:
+                    file0.write(lib.varinput(inst[pos]))
+                pos = pos + 1
+
+        elif inst_name == "var":
+            sov = True
+            pos = lib.position(inst_name, inst) + 1
+            while sov:
+                if inst[pos] == "var.":
+                    sov = False
+                else:
+                    if "*" in inst[pos]:
+                        var_name = inst[pos].split("*")
+                        file0.write(lib.var(var_name[1], 1))
+                    else:
+                        file0.write(lib.var(inst[pos], 0))
+                pos = pos + 1
+        
+        elif "nand" in inst_name:
+            sov = True
+            pos = lib.position(inst_name, inst) + 1
+            while sov:
+                if inst_name == "nand.":
+                    sov = False
+                else:
+                    if pos+3 < len(inst):
+                        if inst[pos + 3] == "nand.":
+                            lib.info("Data correct for "+inst_name)
+                            var1 = inst[pos]
+                            var2 = inst[pos + 1]
+                            var3 = inst[pos + 2]
+                            file0.write(lib.nand(var1, var2, var3))
+                            sov = False
+                pos = pos + 1
+
+        elif inst_name == "print":
+            sov = True
+            pos = lib.position(inst_name, inst) + 1
+            while sov:
+                if inst[pos] == "print.":
+                    sov = False
+                else:
+                    file0.write(lib.printvar(inst[pos]))
+                pos = pos + 1
 
 
 def main():
     file0 = open(sys.argv[1], "r")
-    cleann(file0)
+    inst = clean(file0)
+    sovpile(inst)
+    print(">> Completed <<<")
 
 if __name__=="__main__":
     main()
---
+++ lib.py
@@ -0,0 +1,40 @@
+""" Library for functions """
+
+def nand(var0, var1, result):
+    """ var0 = variable 1
+        var1 = variable 2
+        result = result of nand x and y 
+    """
+    return "if "+var0+"=="+var1+" and "+var0+"==1:\n\t"+result+"=0\nelse:\n\t"+result+"=1\n"
+
+def varinput(name):
+    """ creates variable and gets value with input() """
+    return name+"=int(input(\""+name+" >>> \"))\n"
+
+def var(name, value):
+    """ creates variable and initialices it to 0 """
+    if value == 0:
+        return name+"=0\n"
+    else:
+        return name+"=1\n"
+
+def printvar(name):
+    """ prints given variable """
+    return "print(\"\\n"+name+" >>> \"+str("+name+"))\n"
+
+def info(data):
+    """ prints info """
+    print("Info: "+data)
+
+def position(var0, list):
+    """ returns postion of something in a list """
+    return list.index(var0)
+
+def clean(file):
+    """ divides file in lines and instructions """
+    line = file.read().split("\n")
+    inst = []
+    for x in line:
+        inst.extend(x.split())
+
+    return inst
